4fba91f559b14468bdeb48bca94b2a4180f45a3c
min <minhazur9@gmail.com>
2020-11-17 02:26:27 -0500

    Details page updated

--- details.php
+++ details.php
@@ -2,23 +2,26 @@
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
-$_GET['id'];
-$stmt = $pdo->prepare('SELECT * FROM games WHERE id = 1020');
+$stmt = $pdo->prepare(
+'SELECT *
+FROM amazon
+INNER JOIN games ON games.title = amazon.title
+INNER JOIN gamespot ON gamespot.title = amazon.title 
+WHERE games.id = ?'
+);
     $stmt->execute([$_GET['id']]);
     $game = $stmt->fetch(PDO::FETCH_ASSOC);
+
 	
-$_GET['id'];
-$stmt = $pdo->prepare('SELECT * FROM gamespot WHERE id = 6414475');
-    $stmt->execute([$_GET['id']]);
-    $game2 = $stmt->fetch(PDO::FETCH_ASSOC);
+// $_GET['id'];
+// $stmt = $pdo->prepare('SELECT * FROM gamespot WHERE id = 6414475');
+//     $stmt->execute([$_GET['id']]);
+//     $game2 = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$_GET['id'];
-$stmt = $pdo->prepare('SELECT * FROM amazon WHERE id = 1234');
-    $stmt->execute([$_GET['id']]);
-    $game3 = $stmt->fetch(PDO::FETCH_ASSOC);
-	
-
-
+// $_GET['id'];
+// $stmt = $pdo->prepare('SELECT * FROM amazon WHERE id = 1234');
+//     $stmt->execute([$_GET['id']]);
+//     $game3 = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <?=template_header('Details')?>
@@ -27,9 +30,12 @@ $stmt = $pdo->prepare('SELECT * FROM amazon WHERE id = 1234');
     <h2 class='text-center'><?=$game['title']?></h2>
     <h3 class ='text-center'><?=$game['releaseDate']?></h3>
     <img class="game-cover center" src="<?=$game['cover']?>" alt="cover">
+    <h4 class=text-center>Description</h4>
     <p class='text-center'><?=$game['summary']?><p>
-	<h4 class='text-center'>Game Score: <?=$game2['score']?></h4>
-	<h5 class='text-center'>Amazon Link: <?=$game3['link']?></h5>
+    <h4 class=text-center>Storyline</h4>
+    <p class='text-center'><?=$game['story']?><p>
+	<h4 class='text-center'>Game Score: <?=$game['score']?></h4>
+	<a href=<?= $game['link'] ?>><img class="details-amazon-link" src="/senior-project/images/amazon_button.png" alt=""></a>
 </div>
 
 <?=template_footer()?>
